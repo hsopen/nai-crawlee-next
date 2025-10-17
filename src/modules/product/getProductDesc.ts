2577cc46ac8f8fe9aa08b92ae3f31ba5bdbe23c4
hsopen <hsopen@outlook.com>
2025-10-17 15:07:14 +0800

    🐞 fix: 添加错误处理逻辑到 getProductDesc 函数，确保在获取产品描述失败时返回空字符串

--- src/modules/product/getProductDesc.ts
+++ src/modules/product/getProductDesc.ts
@@ -1,6 +1,11 @@
 import type { Page } from 'playwright'
 
 export async function getProductDesc(page: Page, selector: string) {
-  const productDesc = await page.$eval(`${selector}`, el => el.innerHTML || '')
-  return productDesc
+  try {
+    const productDesc = await page.$eval(`${selector}`, el => el.innerHTML || '')
+    return productDesc
+  }
+  catch {
+    return ''
+  }
 }
